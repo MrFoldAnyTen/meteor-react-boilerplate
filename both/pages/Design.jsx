c675999f2f76cc2241eac826ef7ccdef2c5ed41c
Dylan <dylanf@btinternet.com>
2016-03-01 19:44:39 +0000

    failing to use threejs

--- both/pages/Design.jsx
+++ both/pages/Design.jsx
@@ -1,22 +1,35 @@
+
 Design = React.createClass({
   render() {
     return (
-      <div className='Design'>
+      <div>
+
+      <div id="canvas"></div>
         <h1>Design</h1>
-        {draw()};
+    <button onclick="draw()">Draw</button>
+
       </div>
+
     );
   }
 });
 
+ draw = function(){
 
-var draw = function(){
+        var container = document.getElementById( 'canvas' );
+        document.body.appendChild( container );
+        renderer = new THREE.WebGLRenderer();
+        renderer.setSize( CANVAS_WIDTH, CANVAS_HEIGHT );
+        container.appendChild( renderer.domElement );
+
+
+
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
- document.body.appendChild( renderer.domElement );
+// document.body.appendChild( renderer.domElement );
 //document.getElementById("Design").appendChild( renderer.domElement );
 
 var geometry = new THREE.BoxGeometry( 1, 1, 1 );
